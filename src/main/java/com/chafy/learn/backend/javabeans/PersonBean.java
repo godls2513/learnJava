71d62ed300beec0cd095d265651bf9f03320cfba
chafy <godls2300@gmail.com>
2024-02-21 00:42:07 +0900

    update TestPersonBean and PersonBean

--- src/main/java/com/chafy/learn/backend/javabeans/PersonBean.java
+++ src/main/java/com/chafy/learn/backend/javabeans/PersonBean.java
@@ -2,19 +2,27 @@ package com.chafy.learn.backend.javabeans;
 
 import java.io.*;
 
+/*
+    JavaBeans는 반드시 Serializable 인터페이스를 구현합니다.
+ */
 public class PersonBean implements Serializable {
-    private boolean deceased = false;
+
+    // private으로 필드를 선언합니다.
+    private boolean happiness = false;
     private String name = null;
 
+    // JavaBeans를 사용하기 위해서는 기본생성자가 반드시 필요합니다.
     public PersonBean() {
     }
 
-    public boolean isDeceased() {
-        return deceased;
+    // Getter와 Setter를 이용하여 private으로 선언된 필드에 public으로 접근할 수 있게 합니다.
+
+    public boolean isHappiness() {
+        return happiness;
     }
 
-    public void setDeceased(boolean deceased) {
-        this.deceased = deceased;
+    public void setHappiness(boolean happiness) {
+        this.happiness = happiness;
     }
 
     public String getName() {
--- src/main/java/com/chafy/learn/backend/javabeans/TestPersonBean.java
+++ src/main/java/com/chafy/learn/backend/javabeans/TestPersonBean.java
@@ -4,11 +4,13 @@ public class TestPersonBean {
     public static void main(String[] args) {
         final PersonBean personBean = new PersonBean();
 
+        // Setter를 이용하여 이름과 행복여부를 저장합니다.
         personBean.setName("Bread");
-        personBean.setDeceased(false);
+        personBean.setHappiness(true);
 
+        // Getter를 이용하여 저장된 데이터를 출력합니다.
         System.out.println("Person name is : " + personBean.getName());
-        System.out.println("Person is deceased? : " + personBean.isDeceased());
+        System.out.println("Are you happy? : " + personBean.isHappiness());
 
     }
 }
